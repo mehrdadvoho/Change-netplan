e52089b19b80ffb44e092210e6c1f0a718dc9ce4
Mehrdad <64666641+mehrdadvoho@users.noreply.github.com>
2023-06-22 10:46:36 +0330

    Update netplanDNS.sh

--- netplanDNS.sh
+++ netplanDNS.sh
@@ -2,7 +2,7 @@
 
 cp /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.backup
 
-NETPLAN_FILE="./mm.yaml"
+NETPLAN_FILE="./01-network-manager-all.yaml"
 
 NEW_ADDRESSES=("8.8.8.8" "8.8.4.4")
 
